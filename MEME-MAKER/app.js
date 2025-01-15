1fb5ceb971d5e50af7393c64f7f7b2f94c99731c
SY <foreverhp07@gmail.com>
2025-01-15 23:56:27 +0900

    test

--- MEME-MAKER/app.js
+++ MEME-MAKER/app.js
@@ -2,13 +2,11 @@ const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
-ctx.rect(50, 50, 100, 100);
-ctx.rect(150, 150, 100, 100);
-ctx.rect(250, 250, 100, 100);
-ctx.fill();
 
-ctx.beginPath(); //새 경로로 시작
-ctx.rect(350, 350, 100, 100);
-ctx.rect(450, 450, 100, 100);
-ctx.fillStyle = "red";
-setTimeout(()=>{ctx.fill()},5000);
+ctx.moveTo(50, 50);
+ctx.lineTo(150, 50);
+ctx.lineTo(150, 150);
+ctx.lineTo(50, 150);
+ctx.lineTo(50, 50);
+ctx.stroke();
+ctx.fill();
