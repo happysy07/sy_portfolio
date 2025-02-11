7a48d5105c80743cb5994e667463558e018ffda8
SY <foreverhp07@gmail.com>
2025-02-11 23:38:07 +0900

    cleanFn

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -2,32 +2,28 @@ import Button from "./Button";
 import styles from './App.module.css';
 import { useState, useEffect } from "react";
 
+function Hello(){
+  useEffect(function(){
+    console.log("hi :)")
+    return() => console.log("bye :(");
+  },[])
+  useEffect(()=>{
+      console.log("hi :)")
+      return function(){
+        console.log("bye :(");
+      }
+  }, [])
+  return <h1>Hello</h1>;
+}
+
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  useEffect(()=>{
-      console.log('i run only once')
-  }, []);
-  useEffect(()=>{
-    console.log('i run when keywords changes.')
-  },[keyword])
-  useEffect(()=>{
-    console.log('i run when counter changes.')
-  },[counter])
-  useEffect(()=>{
-    console.log("i run when keyword & counter change")
-  }, [keyword, counter])
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
+
     <div>
-      <input
-        value={keyword} 
-        onChange={onChange} 
-        type="text" 
-        placeholder="search here..."/>
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      {showing ? <Hello/> : null}
+      <button onClick={onClick}>{showing? "Hide" : "Show"}</button>
     </div>
   );
 }
