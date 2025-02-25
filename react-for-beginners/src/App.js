30c2a5f7c6bd1b6e29b874800243275d9d58f431
SY <foreverhp07@gmail.com>
2025-02-26 00:44:24 +0900

    async

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,36 +1,22 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function App(){
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event)=> setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if(toDo === ""){
-      return;
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo("")
-  }
-  console.log(toDos)
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([])
+  const getMovies = async () => {
+    const response = await fetch(
+      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+    )
+    const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+  useEffect(()=>{
+    getMovies();
+  },[])
   return(
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input 
-          onChange={onChange} 
-          value={toDo} 
-          type="text" 
-          placeholder="Wirte your todo...."
-          />
-          <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index)=> (
-          <li key={index}>{item}</li>
-          ))}
-      </ul>
+      {loading? <h1>Loading...</h1> : null }
     </div>
   )
 }
