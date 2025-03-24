58079f3035b6c7636d1a8c95969674645a28d9f1
SY <foreverhp07@gmail.com>
2025-03-24 21:28:04 +0900

    setting

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,24 +1,9 @@
-import { useEffect, useState } from "react";
-
-function App(){
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([])
-  const getMovies = async () => {
-    const response = await fetch(
-      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
-    )
-    const json = await response.json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-  useEffect(()=>{
-    getMovies();
-  },[])
-  return(
+function App() {
+  return (
     <div>
-      {loading? <h1>Loading...</h1> : null }
+      
     </div>
-  )
+  );
 }
 
 export default App;
